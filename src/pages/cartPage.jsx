6f761e8611d6ee8cf9b5dcb724a632071f9ddb3a
bharathi9989 <er.velubharathi.s@gmail.com>
2025-11-17 12:37:41 +0530

    add functionalities

--- src/pages/cartPage.jsx
+++ src/pages/cartPage.jsx
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useContext } from "react";
+import { cartContext } from "../context/CartContext";
 
 function CartPage() {
-  return <div className="text-3xl font-bold text-center">cartPage</div>;
+  const {
+    cart,
+    increaseQty,
+    decreaseQty,
+    addFromCart,
+    RemoveFromCart,
+    totalPrice,
+    finalPrice,
+  } = useContext(cartContext);
+
+  return (
+    <div className="text-3xl font-bold text-center">
+      <h1>Your Cart</h1>
+      {/* If cart is empty */}
+      {cart.length === 0 && <p className="text-gray-600">Your cart is empty</p>}
+
+      {/* cart items */}
+      <div className="space-y-6">
+        {cart.map((item) => (
+          <div
+            key={item.id}
+            className="flex gap-4 p-4 border rounded-xl shadow "
+          >
+            <img
+              src={item.image}
+              alt={item.title}
+              className="w-24 h-24 object-contain"
+            />
+            <div className="flex-1">
+              <h2 className="font-semibold text-lg mb-1">{item.title}</h2>
+              <p className="text-gray-700 text-sm mb-2">{item.price}</p>
+
+              {/* Quantity Control */}
+              <div className="flex items-center gap-4 mb-2">
+                <button
+                  onClick={() => decreaseQty(item.id)}
+                  className="px-3 py-1 bg-gray-300 rounded"
+                >
+                  -
+                </button>
+                <span>{item.quantity}</span>
+                <button
+                  onClick={() => increaseQty(item.id)}
+                  className="px-3 py-1 bg-gray-300 rounded"
+                >
+                  +
+                </button>
+              </div>
+
+              {/* Item Total */}
+
+              <p className="font-semibold">
+                Total : ${(item.price * item.quantity).toFixed(2)}
+              </p>
+              <button
+                onClick={() => RemoveFromCart(item.id)}
+                className="mt-3 text-red-500 font-semibold"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Price Summery */}
+
+      {cart.length > 0 && (
+        <div className="mt-10 p-6 border rounded-xl shadow-lg max-w-md bg-gray-100">
+          <h1 className="text-xl font-bold mb-4">Price Details</h1>
+          <div className="flex justify-between mb-2">
+            <p>Total Price</p>
+            <p>${totalPrice.toFixed(2)}</p>
+          </div>
+
+          <div className="flex justify-between mb-2">
+            <p>Discount 10%</p>
+            <p>${finalPrice.toFixed(2)}</p>
+          </div>
+          <hr className="my-2" />
+          <div className="flex justify-between font-bold text-xl">
+            <p>Final Price</p>
+            <p>${finalPrice.toFixed(2)}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default CartPage;
--- src/pages/productsPage.jsx
+++ src/pages/productsPage.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { cartContext } from "../context/CartContext";
 
 function ProductsPage() {
-  return <div className="text-3xl font-bold text-center">productPage</div>;
+  const [products, setProducts] = useState([]);
+  const { cart, addToCart, removeFromCard } = useContext(cartContext);
+
+  // Fetch products from API
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((res) => setProducts(data));
+  }, []);
+
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">Products</h1>
+
+      {/* Product List */}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product) => {
+          const inCart = cart.find((item) => item.id === product.id);
+
+          return (
+            <div
+              key={product.id}
+              className="border rounded-xl p-4 shadow hover:shadow-lg transition"
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                className="h-40 mx-auto object-contain mb-4"
+              />
+              <h2 className="font-semibold text-lg mb-2 line-clamp-1">
+                {product.title}
+              </h2>
+
+              <p className="text-sm text-gray-600 line-clamp-2 mb-2">
+                {product.describtion}
+              </p>
+              <p className="font-bold text-xl mb-3">{product.price} </p>
+
+              {inCart ? (
+                <button
+                  onClick={() => removeFromCard(product.id)}
+                  className="w-full bg-red-400 text-white py-2 rounded-lg"
+                >
+                  Remove from cart
+                </button>
+              ) : (
+                <button
+                  onClick={() => addToCart(product)}
+                  className="w-full bg-blue-400 text-white py-2 rounded-lg"
+                >
+                  Add to Cart
+                </button>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ProductsPage;
