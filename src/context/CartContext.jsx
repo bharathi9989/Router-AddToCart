1ee58833d27f8e6e34b3feb7f13e85217dcc1b69
bharathi9989 <er.velubharathi.s@gmail.com>
2025-11-17 11:20:25 +0530

    add Context

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,71 @@
+import { createContext, useState } from "react";
+
+export const cartContext = createContext();
+
+export function CartProvider({ childer }) {
+  const [cart, setCart] = useState([]);
+
+  //  Add product to cart
+
+  const addToCart = (product) => {
+    const existingProduct = cart.find((item) => item.id === product.id);
+
+    if (!existingProduct) {
+      setCart([...cart], { ...product, quantity: 1 });
+    }
+  };
+
+  // Remove product from cart
+
+  const removeFromCart = (product) => {
+    setCart(cart.filter((item) => item.id !== id));
+  };
+
+  // Increase quantity of
+  const increaseQty = (id) => {
+    setCart(
+      cart.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  // Decrease Quantity
+
+  const decreaseQty = (id) => {
+    setCart(
+      cart.map((item) =>
+        item.id === id && item.quantity > 1
+          ? { ...cart, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
+  // Total price before discound
+
+  const totalPrice = cart.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  // final total after 10% discount
+
+  const finalPrice = totalPrice - totalPrice * 0.1;
+
+  return (
+    <cartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        increaseQty,
+        decreaseQty,
+        totalPrice,
+        finalPrice,
+      }}
+    >
+      {childer}
+    </cartContext.Provider>
+  );
+}
